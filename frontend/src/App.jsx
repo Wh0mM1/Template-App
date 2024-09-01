ed4d959e9ef0b8491b162712209026326d5f8411
Wh0mM1 <shaikhnihal92@gmail.com>
2024-09-01 15:54:38 +0530

    clean ui

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,9 +4,9 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
 import { DefineFields } from "./componets/DefineFields/DefineFields";
 import { TemplateScreen } from "./componets/TemplateScreen/TemplateScreen";
-import { AddOrg } from "./componets/AddOrg";
-import { AddUser } from "./componets/AddUser";
-import { EditFileds } from "./componets/EditFields";
+import { AddOrg } from "./componets/AddOrg/AddOrg";
+import { AddUser } from "./componets/AddUser/AddUser";
+import { EditFields } from "./componets/EditFields/EditFields";
 
 function App() {
   return (
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<DefineFields />} />
           <Route path="/add-org" element={<AddOrg />} />
           <Route path="/add-user" element={<AddUser />} />
-          <Route path="/edit-fields" element={<EditFileds />} />
+          <Route path="/edit-fields" element={<EditFields />} />
           <Route path="/template-screen" element={<TemplateScreen />} />
         </Routes>
       </BrowserRouter>
